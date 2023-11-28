26648aefe23ec4c26891f867f5617d1bda0b22b5
sourav sambit behera <souravsambit12@gmail.com>
2023-11-28 15:32:20 +0530

    done task

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Modal, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Product_datas } from "../Product_datas";
+import {
+  Styled_Box_Div,
+  Styled_Card,
+  Styled_FliterImg_Section,
+  Styled_Products_li,
+  Styled_Products_ul,
+} from "./AppStyled";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 800,
+  height:600,
+  // minHeight: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [open, setOpen] = useState(false);
+  const [filterProducts, setFilterProducts] = useState(Product_datas);
+  const [filterImages, setfilterImages] = useState([]);
 
-export default App
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handleProductDataClick = (product) => {
+    // const data =
+    const data = filterProducts.filter((item) => item.id !== product.id);
+    setFilterProducts(data);
+    // console.log(data)
+    setfilterImages([product , ...filterImages]);
+  };
+  // console.log(filterImages);
+  const handleDeleteClick =(product)=>{
+    const data = filterImages.filter((item) => item.id !== product.id);
+
+    setFilterProducts([
+      product,...filterProducts
+    ])
+    setfilterImages(data)
+  }
+  return (
+    <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Styled_Box_Div>
+            <Styled_Products_ul>
+              {filterProducts.map((product) => (
+                <Styled_Products_li
+                  key={product.id}
+                  onClick={() => handleProductDataClick(product)}
+                >
+                  {product.title.slice(0, 4)}...
+                </Styled_Products_li>
+              ))}
+            </Styled_Products_ul>
+
+
+            <Styled_FliterImg_Section>
+              {filterImages.map((product) =>
+                // console.log(product)
+              
+              (
+                <Styled_Card sx={{ width: 170, height:150 }} key={product.id}>
+                {/* <CardMedia
+                  sx={{ height: 110, 
+                  width:110
+                  }}
+                  image={product.image}
+                  title={product.title}
+                /> */}
+                <img src={product.image} alt={product.title} width="100px" height="100px"/>
+                
+                <CardActions>
+                  <Button size="small" onClick={()=>handleDeleteClick(product)}>Delete</Button>
+                  <Button size="small">See More</Button>
+                </CardActions>
+              </Styled_Card>
+              ))}
+            </Styled_FliterImg_Section>
+          </Styled_Box_Div>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/AppStyled.js
@@ -0,0 +1,62 @@
+import { Card, CardMedia } from "@mui/material";
+import styled from "styled-components";
+
+export const Styled_Box_Div = styled.div`
+    display: flex;
+    justify-content: space-between;
+  /* background-color: aqua; */
+  color: #BF4F74;
+  /* box-sizing: border-box; */
+  padding: 4px 5px;
+    margin: 5px 9px;
+    gap: 10px;
+  /* margin-bottom: 13px; */
+  /* box-shadow: 0 0px 12px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);   */
+  /* cursor: pointer; */
+   /* Safari */
+  /* transition-duration: 0.3s; */
+
+
+`
+export const Styled_Products_ul = styled.ul`
+list-style-type:none;
+`
+export const Styled_Products_li = styled.li`
+padding: 2px;
+    color:#0065f2fc;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-size: 16px;
+    font-weight:600;
+    text-transform: uppercase;
+    cursor: pointer;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.3s;
+    &:hover,
+  &:focus {
+    color: #009bf2de;
+  box-shadow: 0 0px 0px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);  
+  }
+`
+export const Styled_FliterImg_Section = styled.section`
+  /* min-width: 620px;  */
+  max-height: 480px;
+     padding: 8px 10px; 
+     margin: 10px 18px; 
+     display: flex;
+     flex-wrap: wrap;
+     overflow-y:auto;
+     gap: 20px;
+`
+export const Styled_Card = styled(Card)({
+    display:"flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    cursor:"pointer"
+
+
+  
+  })
+export const Styled_CardMedia = styled(CardMedia)({
+    display:"flex",
+
+})
